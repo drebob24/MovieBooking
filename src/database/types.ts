4ff342bb259bc2cd36e4250d65f4c75e9ffb4ed8
Andrius Bobbit <abobbit@gmail.com>
2025-06-03 11:12:40 +0000

    Create and Run Migrations for Users and Reservations Tables

--- src/database/types.ts
+++ src/database/types.ts
@@ -1,49 +1,65 @@
-import type { ColumnType } from "kysely";
+import type { ColumnType } from 'kysely'
 
-export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>;
+export type Generated<T> =
+  T extends ColumnType<infer S, infer I, infer U>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>
 
 export interface Directors {
-  movieId: number;
-  personId: number;
+  movieId: number
+  personId: number
 }
 
 export interface Movies {
-  id: number | null;
-  title: string;
-  year: number | null;
+  id: number | null
+  title: string
+  year: number | null
 }
 
 export interface People {
-  birth: number | null;
-  id: number | null;
-  name: string;
+  birth: number | null
+  id: number | null
+  name: string
 }
 
 export interface Ratings {
-  movieId: number;
-  rating: number;
-  votes: number;
+  movieId: number
+  rating: number
+  votes: number
+}
+
+export interface Reservations {
+  id: Generated<number>
+  screeningId: number
+  tickets: number
+  userId: number
 }
 
 export interface Screenings {
-  date: number;
-  id: Generated<number>;
-  movieId: number;
-  seats: number;
+  date: number
+  id: Generated<number>
+  movieId: number
+  seats: number
 }
 
 export interface Stars {
-  movieId: number;
-  personId: number;
+  movieId: number
+  personId: number
+}
+
+export interface Users {
+  id: Generated<number>
+  password: string
+  username: string
 }
 
 export interface DB {
-  directors: Directors;
-  movies: Movies;
-  people: People;
-  ratings: Ratings;
-  screenings: Screenings;
-  stars: Stars;
+  directors: Directors
+  movies: Movies
+  people: People
+  ratings: Ratings
+  reservations: Reservations
+  screenings: Screenings
+  stars: Stars
+  users: Users
 }
